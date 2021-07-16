0e3203b13d7b488ba940e2993e24074ab60426f7
Mitsubishi <salus@MitsubishinoMacBook-Pro.local>
2021-07-16 22:06:47 +0900

    feat: 🎸 検索バーコンポーネントの作成
    フォルダ構成も変更したため若干余計なファイルも更新あり

--- lib/view/screens/pages/news_list_page.dart
+++ lib/view/screens/pages/news_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_feed/view/components/search_bar.dart';
 
 class NewsListPage extends StatelessWidget {
   const NewsListPage({Key? key}) : super(key: key);
@@ -10,12 +11,15 @@ class NewsListPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
         onPressed: () => onRefresh(),
+        tooltip: "更新",
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            // SearchBar(),
+            SearchBar(
+              onSearch: (keyword)=>geKeywordNews(context,keyword),
+            ),
             // CategoryChips(),
             Expanded(child: Center(child: CircularProgressIndicator()))
           ],
@@ -23,6 +27,13 @@ class NewsListPage extends StatelessWidget {
       ),
     ));
   }
-
-  onRefresh() {}
+  //TODO: 記事更新処理
+  onRefresh() {
+    print("onRefresh");
+  }
+  //TODO: キーワード記事検索取得
+  geKeywordNews(BuildContext context, keyword) {
+    print("getkeywordnews");
+    print(keyword);
+  }
 }
