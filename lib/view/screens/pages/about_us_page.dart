936a57df0503ec51b0ca3a9ca7c6874197f8c608
Mitsubishi <salus@MitsubishinoMacBook-Pro.local>
2021-07-20 14:31:01 +0900

    feat: 🎸 create about page
    オートテキストサイズとアニメーションを利用

--- lib/view/screens/pages/about_us_page.dart
+++ lib/view/screens/pages/about_us_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsPage extends StatefulWidget {
@@ -8,11 +9,33 @@ class AboutUsPage extends StatefulWidget {
 }
 
 class _AboutUsPageState extends State<AboutUsPage> {
+  bool _selected = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        child: Text("about"),
+      child: Scaffold(
+        body: Center(
+          child: AnimatedContainer(
+            // color: _selected ? Colors.black26 : Colors.redAccent,
+            width: _selected ? 400 : 50,
+            height: _selected ? 300 : 25,
+            child: AutoSizeText(
+              "NEWS FEED APP",
+              style: TextStyle(fontSize: 40),
+              minFontSize: 6,
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+            ),
+            duration: Duration(seconds: 1),
+            alignment: _selected ? Alignment.center : Alignment.topCenter,
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: "飛び出すよ",
+          child: Icon(Icons.play_arrow),
+          onPressed: () => setState(() => _selected = !_selected),
+        ),
       ),
     );
   }
